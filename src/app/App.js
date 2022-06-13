978745fe9ab585011f098a2f73c0312367323eb8
jam65st <jam65st@gmail.com>
2022-06-13 12:40:27 -0500

    Updating features (header, footer, alarm)

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,32 @@
-import React     from 'react';
-import Pomodoro  from "./utils/Pomodoro";
-import ExtraData from "./utils/ExtraData";
+import React      from 'react';
+import Pomodoro   from "./utils/Pomodoro";
+import ExtraData  from "./utils/ExtraData";
+import AppTitle   from "./components/AppTitle";
+import AuthorInfo from "./components/AuthorInfo";
+import BeepAlarm  from "./components/BeepAlarm";
 
 
 const App = () => {
 	
 	return (
-			<div>
+			<section id="clock">
+				{ /** Working OK */ }
+				<AppTitle title={ ExtraData.getProjectTitle() }/>
+				
+				
+				{ /** BEGIN HERE !!! */ }
+				
 				Hello-world { Pomodoro.SESSION_TITLE }
-				{ ExtraData.getProjectTitle() }
 				test: { Pomodoro.BREAK_TITLE }
-			</div>
+				
+				{ /** END HERE!!! */ }
+				
+				{ /** Working OK */ }
+				<AuthorInfo authorInfo={ ExtraData.getProjectAuthor() }/>
+				
+				{ /*TODO: May be could need the timer on the BeepAlarm props */ }
+				<BeepAlarm alarm={ ExtraData.getExternalResource( 'alarm' ) }/>
+			</section>
 	);
 }
 
